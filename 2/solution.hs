63fcdf5decca670a859c775852456abbb4f52045
Josh <josh@nplan.io>
2021-12-02 10:30:56 +0000

    slightly cleaner solution to day 2

--- 2/solution.hs
+++ 2/solution.hs
@@ -2,25 +2,23 @@
 part1 :: [String] -> Int -> Int -> Int
 part1 [] h d = h * d
 part1 (c:cs) h d 
-        | cmd == "forward" = part1 cs (h + x) d
-        | cmd == "down"    = part1 cs h (d + x)
-        | cmd == "up"      = part1 cs h (d - x)
+        | cmd == "forward" = part1 cs (h + read x) d
+        | cmd == "down"    = part1 cs h (d + read x)
+        | cmd == "up"      = part1 cs h (d - read x)
         | otherwise        = part1 cs h d -- Skip if unrecognised
     where
-        cmd = head (words c)
-        x   = read (head (tail (words c)))
+        [cmd, x] = words c
 
 -- Commands -> Aim -> Height -> Depth -> Returns: Height * Depth 
 part2 :: [String] -> Int -> Int -> Int -> Int
 part2 [] _ h d = h * d
 part2 (c:cs) a h d 
-        | cmd == "down"    = part2 cs (a + x) h d
-        | cmd == "up"      = part2 cs (a - x) h d
-        | cmd == "forward" = part2 cs a (h + x) (d + (a * x))
+        | cmd == "down"    = part2 cs (a + read x) h d
+        | cmd == "up"      = part2 cs (a - read x) h d
+        | cmd == "forward" = part2 cs a (h + read x) (d + (a * read x))
         | otherwise        = part2 cs a h d -- Skip if unrecognised
     where
-        cmd = head (words c)
-        x   = read (head (tail (words c)))
+        [cmd, x] = words c
 
 main :: IO()
 main = do
